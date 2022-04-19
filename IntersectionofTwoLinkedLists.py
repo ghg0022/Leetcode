6a4b411f3db5887689df2e36cec2f5093fe22983
ghg0022 <103839980+ghg0022@users.noreply.github.com>
2022-04-20 03:10:50 +0900

    Update IntersectionofTwoLinkedLists.py

--- IntersectionofTwoLinkedLists.py
+++ IntersectionofTwoLinkedLists.py
@@ -1,5 +1,3 @@
-"""160. Intersection of Two Linked Lists"""
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -7,4 +5,21 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    def getIntersectionNode(self, headA, headB):
+        
+        if headA is None or headB is None:
+            return None # 빈 연결리스트인지 확인
+        
+# headA의 마지막 노드에 도달 시, headB의 첫번째 노드와 연결
+# 반대의 경우도 마찬가지로 headB의 마지막 노드에 도달 시, headA의 첫번째 노드와 연결
+# headA = headB인 경우가 생긴다면 intersectionNode가 존재함
+# ex) 4 1 8 4 5   5 6 1 8 4 5
+#     5 6 1 8 4 5   4 1 8 4 5   // 1845가 겹침, 1이 intersectionNode
+
+        pa, pb = headA, headB
+
+        while pa != pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa
