bd73f459557665305c68e4c33bef00d1b2433d0e
ghg0022 <103839980+ghg0022@users.noreply.github.com>
2022-04-28 15:38:54 +0900

    Update FindtheWinneroftheCircularGame.py

--- FindtheWinneroftheCircularGame.py
+++ FindtheWinneroftheCircularGame.py
@@ -18,10 +18,10 @@ class Solution:
         self.queue1 = deque() # deque 정의
 
         for i in range(1, n+1):
-            self.queue1.append(i) # deque에 n만큼의 원소를 맨 끝(오른쪽)부터 넣어줌 ex) n=5일때, 1 2 3 4 5
+            self.queue1.append(i) # deque에 n만큼의 원소를 맨 끝(오른쪽)부터 넣어줌 ex) n=5일때, 1 2 3 4 5 O(n)
         
-        while self.queue1:
-            temp = k - 1 # k번째의 사람이 게임에서 떠나야하므로 k-1번째까지 실행
+        while self.queue1: # O(n*k)???
+            temp = k - 1 # k번째의 사람이 게임에서 떠나야하므로 k-1번째까지 실행 O(k)
             while temp > 0:
                 self.queue1.append(self.queue1.popleft()) # k-1번째까지의 원소를 deque 오른쪽에 다시 넣어줌
                 temp -= 1
